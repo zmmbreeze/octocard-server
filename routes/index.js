2c3c1007e3bd7f72e8ec80a15c2059b8b98a5ac5
zmmbreeze <zmmbreeze0825@gmail.com>
2013-10-07 18:41:18 +0800

    Modified index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
+/*jshint laxbreak:true */
 
 /*
  * GET home page.
  */
 
-var index = function(req, res){
-    res.render('index', { title: 'Express' });
+var index = function (req, res) {
+    res.render('index', {
+        title: 'Octocard',
+        loginName: req.session.loginName || 'your github username'
+    });
+};
+
+module.exports = function (app) {
+    app.get('/', index);
 };
